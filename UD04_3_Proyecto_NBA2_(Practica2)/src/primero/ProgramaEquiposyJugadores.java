68d20d7fafa6d57371e7ce0fc7cb5a14c89e7e5f
DAM2Alu10 <210176@iessierraguara.es>
2022-11-17 12:43:36 +0100

    Practica2 terminada

---
+++ UD04_3_Proyecto_NBA2_(Practica2)/src/primero/ProgramaEquiposyJugadores.java
@@ -0,0 +1,49 @@
+package primero;
+
+import java.text.DecimalFormat;
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+public class ProgramaEquiposyJugadores {
+
+	public static void main(String[] args) {
+		SessionFactory sesion = SessionFactoryUtil.getSessionFactory();
+		Session session;
+		session = sesion.openSession();
+		List<Equipos> lista = session.createQuery("FROM Equipos").list();
+		
+		System.out.println("Número de equipos: " + lista.size());
+		System.out.println("=============================");
+		for (Equipos eq : lista) {
+			Equipos e = (Equipos) session.get(Equipos.class, (String) eq.getNombre());
+			System.out.println("Equipo: " + e.getNombre());
+			for(Object o:e.getJugadoreses()) {
+				String s="";
+				Jugadores j=(Jugadores)o;
+				s=j.getCodigo()+", "+j.getNombre();				
+				double pts=0;
+				int cont=0;
+				for(Object ob:j.getEstadisticases()) {
+					Estadisticas es=(Estadisticas)ob;
+					pts+=es.getPuntosPorPartido();
+					cont++;
+				}
+				double mediaD=pts/cont;
+				String media;
+				if(mediaD==0)media="0";
+				
+				else if(mediaD>0) {
+				DecimalFormat df = new DecimalFormat("#.00");
+				media=df.format(pts/cont);}
+				else media="Sin registros";
+				s+=", "+media;
+				System.out.println(s);
+				
+			}System.out.println("============================");
+		}
+	}
+
+	
+}
